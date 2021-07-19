d8cfa0415eea7a4dc2267dff4bcb09a3050f8113
Zaid Salah <zaidsalah351@gmail.com>
2021-07-19 10:34:41 +0300

    makeing api request in vue way

---
+++ pages/posts/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="h-full">
+        <h1>Posts Page</h1>
+        <div>
+            <h5 v-for="post in posts" :key="post.id">{{post.title}}</h5>
+        </div>
+
+    </div>
+</template>
+
+<script>
+// import axios from "@nuxtjs/axios"
+export default {
+    data() {
+        return {
+            posts: []
+        }
+    },
+    // this way is not server side rendered approach!!! just single page applicaiton
+    mounted() {
+        // because i have installed axios nuxt so i can use like this or we can install axios from npm install axios then import it and use it
+        this.$axios.get("https://jsonplaceholder.typicode.com/todos")
+        .then(res => {
+            this.posts = res.data;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+}
+</script>
