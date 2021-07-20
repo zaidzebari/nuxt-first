84308084d5006e9490f197b01883784e4179eb0d
Zaid Salah <zaidsalah351@gmail.com>
2021-07-20 10:54:37 +0300

    used vue-select(no SSR support) plugin

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <Home/>
+  <div>
+    <Home />
+   <div>
+      <v-select v-model="selected" placeholder="Select Category" :options="['one', 'two']"></v-select>
+   </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      selected: ''
+    }
+  }
 }
 </script>
