a2771e360ee0241e7fb79bf2e12cbd166c345f40
Zaid Salah <zaidsalah351@gmail.com>
2021-07-20 10:48:29 +0300

    registered vue-select(no SSR support) plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/scrollto.js'],
+  plugins: ['@/plugins/scrollto.js', '@/plugins/vueselect.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
