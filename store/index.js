925d026abdabb166257f3e9910d81fa1403a36b8
Zaid Salah <zaidsalah351@gmail.com>
2021-07-20 11:25:08 +0300

    commit used as a parameter

--- store/index.js
+++ store/index.js
@@ -17,9 +17,10 @@ export const mutations = {
 // actions
 export const actions = {
     // this method we can use inside vuex store application just by the name we can quest this method was initialized on the server side every time when application bootsup and get ready this method will run before application rendered for the client this can be useful to load post so when application is ready and before to render to user nuxt server init will be initialize 
-    async nuxtServerInit() {
+    async nuxtServerInit({commit}) {
         const res = await this.$axios.$get('https://jsonplaceholder.typicode.com/posts');
-        this.commit('SET_POST', res);
+        commit('SET_POST', res);
+        // this.commit('SET_POST', res); // without the paramter
     }
     // setPosts({commit}, posts) {
     //     commit('SET_POST', posts);
