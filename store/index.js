4681120784f45917f471e2d283bb57227133e7d4
Zaid Salah <zaidsalah351@gmail.com>
2021-07-20 11:21:45 +0300

    what is nuxtServerInit()

--- store/index.js
+++ store/index.js
@@ -16,6 +16,7 @@ export const mutations = {
 }
 // actions
 export const actions = {
+    // this method we can use inside vuex store application just by the name we can quest this method was initialized on the server side every time when application bootsup and get ready this method will run before application rendered for the client this can be useful to load post so when application is ready and before to render to user nuxt server init will be initialize 
     async nuxtServerInit() {
         const res = await this.$axios.$get('https://jsonplaceholder.typicode.com/posts');
         this.commit('SET_POST', res);
