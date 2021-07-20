db2f6e63851458ef897b1a498d9fcc808d9bd017
Zaid Salah <zaidsalah351@gmail.com>
2021-07-20 11:05:40 +0300

    vue-select(no SSR support) done

--- components/Home.vue
+++ components/Home.vue
@@ -2,8 +2,5 @@
 <template>
   <div class="flex justify-center items-center w-full h-full">
       this is home page
-       <client-only>
-      <v-select v-model="selected" placeholder="Select Category" :options="['one', 'two']"></v-select>
-      </client-only>
   </div>
 </template>
