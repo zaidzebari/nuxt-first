ad00f852228e51d49bd6092b59e5919936288135
Zaid Salah <zaidsalah351@gmail.com>
2021-07-20 10:34:19 +0300

    vue scroll done know we can use it

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,8 +30,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: ['@/plugins/scrollto.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
---
+++ plugins/scrollto.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import VueScrollTo from 'vue-scrollto';
+
+//use the plugin
+Vue.use(VueScrollTo);
+
