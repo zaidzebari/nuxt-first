a6d46647d7998bcebd515354d1c9fb0c60bd1706
Zaid Salah <zaidsalah351@gmail.com>
2021-07-21 16:51:41 +0300

    removed unused tailwind css class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./components/**/*.vue","./layouts/**/*.vue","./pages/**/*.vue"],
+  purge: ["./components/**/*.vue","./layouts/**/*.vue","./pages/**/*.vue"],// only use this class
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
