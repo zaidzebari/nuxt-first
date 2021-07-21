39d9f11a05a275530d8dc7dd9fddd98bbb6c5bcb
Zaid Salah <zaidsalah351@gmail.com>
2021-07-21 16:46:05 +0300

    added purge for tailwid css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ["./components/**/*.vue","./layouts/**/*.vue","./pages/**/*.vue"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
